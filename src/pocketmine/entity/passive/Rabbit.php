d502eabd3beddf322ee4533dc950907dc7cc1329
dearminder <androidwiktor@gmail.com>
2019-08-03 16:44:55 +0200

    CTRL + ALT + L

--- src/pocketmine/entity/passive/Rabbit.php
+++ src/pocketmine/entity/passive/Rabbit.php
@@ -39,20 +39,19 @@ use function mt_rand;
 class Rabbit extends Tamable{
 	public const NETWORK_ID = self::RABBIT;
 
-	public $width =  0.4;
+	public $width = 0.4;
 	public $height = 0.5;
 
-	protected function addBehaviors() : void
-    {
-        $this->behaviorPool->setBehavior(0, new FloatBehavior($this));
-        $this->behaviorPool->setBehavior(1, new MateBehavior($this, 2.0));
-        $this->behaviorPool->setBehavior(2, new PanicBehavior($this, 2.0));
-        $this->behaviorPool->setBehavior(3, new LookAtPlayerBehavior($this, 14.0));
-        $this->behaviorPool->setBehavior(4, new RandomLookAroundBehavior($this));
-        $this->behaviorPool->setBehavior(5, new TemptBehavior($this, [Item::CARROT], 1.0));
-	    $this->behaviorPool->setBehavior(6, new AvoidMobTypeBehavior($this, Player::class, null, 8, 1, 1));
-        // TODO: going back to player when they see carrot, and movement fix so they dont SLIDE On ground
-    }
+	protected function addBehaviors() : void{
+		$this->behaviorPool->setBehavior(0, new FloatBehavior($this));
+		$this->behaviorPool->setBehavior(1, new MateBehavior($this, 2.0));
+		$this->behaviorPool->setBehavior(2, new PanicBehavior($this, 2.0));
+		$this->behaviorPool->setBehavior(3, new LookAtPlayerBehavior($this, 14.0));
+		$this->behaviorPool->setBehavior(4, new RandomLookAroundBehavior($this));
+		$this->behaviorPool->setBehavior(5, new TemptBehavior($this, [Item::CARROT], 1.0));
+		$this->behaviorPool->setBehavior(6, new AvoidMobTypeBehavior($this, Player::class, null, 8, 1, 1));
+		// TODO: going back to player when they see carrot, and movement fix so they dont SLIDE On ground
+	}
 
 	protected function initEntity() : void{
 		$this->setMaxHealth(3);
@@ -68,15 +67,14 @@ class Rabbit extends Tamable{
 	}
 
 
+	public function getXpDropAmount() : int{
+		return rand(1, 3);
+	}
 
-        public function getXpDropAmount() : int{
-            return rand(1, 3);
-        }
-
-    public function getDrops() : array{
-        return [
-            ItemFactory::get(Item::RABBIT_HIDE, 0, rand(0, 1)),
-            ($this->isOnFire() ? ItemFactory::get(Item::COOKED_RABBIT, 0, rand(0, 1)) : ItemFactory::get(Item::RAW_RABBIT, 0, rand(0, 1)))
-        ];
+	public function getDrops() : array{
+		return [
+			ItemFactory::get(Item::RABBIT_HIDE, 0, rand(0, 1)),
+			($this->isOnFire() ? ItemFactory::get(Item::COOKED_RABBIT, 0, rand(0, 1)) : ItemFactory::get(Item::RAW_RABBIT, 0, rand(0, 1)))
+		];
 	}
 }
